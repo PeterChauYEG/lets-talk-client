bc3a4aa2e0dc873ff8009a82b050c3ae7b6249c3
PeterChauYEG <peter.chau.esq@gmail.com>
2018-03-23 22:43:26 -0600

    created home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="header">
+          <h1 className="title">MMOR</h1>
+          <h1 className="username">Username</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="stream-container">
+          <img className="stream"
+            src="http://via.placeholder.com/500x500" />
+        </div>
+
+        <div className="body-container">
+          <p className="queue-position">
+            Queue Position: Not in Queue
+          </p>
+        </div>
+
+        <div className="button-container">
+          <div className="button">
+            Join Queue
+          </div>
+          <div className="button">
+            Leaderboard
+          </div>
+        </div>
       </div>
     );
   }
