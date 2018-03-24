a60c17f6814f063c25352fad6523b1cb6953a109
PeterChauYEG <peter.chau.esq@gmail.com>
2018-03-24 16:02:53 -0600

    hooked in socket io for drive command

--- src/components/Race.js
+++ src/components/Race.js
@@ -6,6 +6,9 @@ import { store } from '../redux';
 // navigation
 import { push } from 'react-router-redux';
 
+// socket
+import io from 'socket.io-client'
+
 // styles
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faCaretUp from '@fortawesome/fontawesome-free-solid/faCaretUp'
@@ -14,7 +17,14 @@ import faCaretLeft from '@fortawesome/fontawesome-free-solid/faCaretLeft'
 import faCaretRight from '@fortawesome/fontawesome-free-solid/faCaretRight'
 import './Race.css';
 
+// socket
+const socket = io('http://localhost:8080')
+
 class Race extends Component {
+  handleControl(direction) {
+    socket.emit('gpio', direction);
+  }
+
   render() {
     return (
       <div className="Race">
@@ -34,12 +44,24 @@ class Race extends Component {
             alt="stream" />
         </div>
         <div className="Race-left-control-container">
-          <FontAwesomeIcon icon={faCaretUp} size="4x" />
-          <FontAwesomeIcon icon={faCaretDown} size="4x" />
+          <FontAwesomeIcon
+            onClick={() => this.handleControl('forward')}
+            icon={faCaretUp}
+            size="4x" />
+          <FontAwesomeIcon
+            onClick={() => this.handleControl('backward')}
+            icon={faCaretDown}
+            size="4x" />
         </div>
         <div className="Race-right-control-container">
-          <FontAwesomeIcon icon={faCaretLeft} size="4x" />
-          <FontAwesomeIcon icon={faCaretRight} size="4x" />
+          <FontAwesomeIcon
+            onClick={() => this.handleControl('left')}
+            icon={faCaretLeft}
+            size="4x" />
+          <FontAwesomeIcon
+            onClick={() => this.handleControl('right')}
+            icon={faCaretRight}
+            size="4x" />
         </div>
       </div>
     );
